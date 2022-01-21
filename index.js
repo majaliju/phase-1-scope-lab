14b029c9d04f7c67164560ef6094026b6a9b076f
majaliju <majalijunyc@gmail.com>
2022-01-20 19:09:20 -0500

    completed (?)

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName;
+
+function upperCaseCustomerName(){
+    return customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    var bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    var bestCustomer = 'somebody else';
+}
+
+const leastFavoriteCustomer = 'jessica';
+
+function changeLeastFavoriteCustomer(){
+    const leastFavoriteCustomer = 'brittany';
+    return leastFavoriteCustomer;
+}
